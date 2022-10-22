b2f646714cb25a9aac39ede8de9f253efdd55dab
Jane Awuah <janeawuah@gmail.com>
2022-10-22 21:21:06 +0000

    added files to generate passwords, I'll continue tomorrow, I'm sleepy

--- countdown_timer/countdown_timer.py
+++ countdown_timer/countdown_timer.py
@@ -1,15 +1,13 @@
 import time
 
-
-def countdown(time_entered):
-    
+def count_down(time_entered):
     while time_entered:
         mins, secs = divmod(time_entered, 60)
+        timer = '{'
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
-        time_entered -= 1
-      
+        time_entered -= 1 
     print('Times UP!!!!!')
   
   
@@ -17,4 +15,4 @@ def countdown(time_entered):
 time_entered = input("Please enter the time in seconds: ")
   
 # function call
-countdown(int(time_entered))
+count_down(int(time_entered))
